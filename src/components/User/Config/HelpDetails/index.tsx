49b13c2a9c6a51177c90d1a9ef5238a61942f963
Adson San Dias <adsonbmx18@gmail.com>
2022-08-11 11:12:53 -0300

    Pre added config pages styles

--- src/components/User/Config/HelpDetails/index.tsx
+++ src/components/User/Config/HelpDetails/index.tsx
@@ -1,10 +1,34 @@
-import { Context } from "./styles";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as ArrowIcon } from "../../../../assets/arrow-icon.svg";
+import { ContainerAnimation, Context } from "./styles";
 
 export function HelpDetails() {
   return (
     <Context>
-      <h1>Configuração</h1>
-      <h2>Help Details</h2>
+      <header>
+        <nav aria-label="Navigate to Help Details">
+          <ul>
+            <li>
+              <Link to="/user">
+                <ArrowIcon />
+                <span>Help</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <h2>Detalhes de Ajuda</h2>
+      </header>
+      <ContainerAnimation>
+        <Player
+          autoplay
+          loop
+          src="https://assets3.lottiefiles.com/packages/lf20_w1fl6e19.json"
+          style={{ height: "300px", width: "300px" }}
+        />
+        <p>Estamos nos esforçando para terminar essa página.</p>
+      </ContainerAnimation>
     </Context>
   );
 }
--- src/components/User/Config/HelpDetails/styles.ts
+++ src/components/User/Config/HelpDetails/styles.ts
@@ -6,10 +6,94 @@ export const Context = styled.div`
   background: white;
   margin: 0 auto;
   flex: 1;
-  display: flex;
-  flex-direction: column;
+  display: grid;
   width: 100%;
   margin-top: -8rem;
   margin-bottom: 7rem;
   border-radius: 1.56rem;
+  position: relative;
+
+  header {
+    position: absolute;
+    grid-area: -1/1;
+  }
+
+  nav {
+    ul {
+      list-style: none;
+
+      li {
+        a {
+          display: flex;
+          align-items: center;
+          text-decoration: none;
+
+          svg {
+            transform: rotate(180deg);
+          }
+
+          span {
+            margin-left: 1rem;
+            font-size: 1.63rem;
+            color: var(--color-text-2);
+            @media (max-width: 480px) {
+              font-size: 1.38rem;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  h2 {
+    margin-top: 2rem;
+    font-size: 1rem;
+    color: var(--color-text-2);
+    @media (max-width: 480px) {
+      font-size: 1.38rem;
+    }
+  }
+
+  @media (max-width: 1180px) {
+    width: initial;
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+  @media (max-width: 830px) {
+    width: initial;
+    padding: 2rem;
+    grid-template-columns: 1fr;
+    box-shadow: initial;
+    border-radius: 0.94rem;
+  }
+`;
+
+export const ContainerAnimation = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  grid-area: -1/1;
+
+  p {
+    width: 25ch;
+    margin-top: 1rem;
+    text-align: center;
+    bottom: 20%;
+  }
+
+  #lottie {
+    pointer-events: none !important;
+  }
+
+  @media (max-width: 830px) {
+    grid-area: initial;
+  }
+
+  @media (max-width: 480px) {
+    #lottie {
+      width: 150px !important;
+      height: 150px !important;
+    }
+  }
 `;
