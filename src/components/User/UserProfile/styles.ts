ecb313e94bec2af48e32bd0f1c2476ee81755982
Adson San Dias <adsonbmx18@gmail.com>
2022-08-19 23:46:33 -0300

    Added Frame Motion

--- src/components/User/UserProfile/styles.ts
+++ src/components/User/UserProfile/styles.ts
@@ -1,6 +1,7 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
-export const Container = styled.section`
+export const Container = styled(motion.section)`
   padding: 4rem;
   max-width: 1120px;
   background: white;
