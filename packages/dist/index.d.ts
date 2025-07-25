8eef48392fccc345dd9d8b8734da50746fa96e6c
Adson Macbook Air <adsonbmx18@gmail.com>
2025-07-25 14:48:43 -0300

    chore: clean up backend dist files and update gitignore
    Remove outdated backend distribution files and add dist/.env.example to gitignore

--- packages/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './shared-types/src';
-export * from './constants';
-export * from './shared-utils/src/DependencyContainer';
--- packages/dist/shared-types/src/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Transaction related types
- */
-export var TransactionType;
-(function (TransactionType) {
-    TransactionType["INCOME"] = "income";
-    TransactionType["EXPENSE"] = "expense";
-})(TransactionType || (TransactionType = {}));
